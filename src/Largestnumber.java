15a007626d481c003818465655cd50cdded549a6
susighnitha <susighnithareddy00@gmail.com>
2021-07-09 08:32:50 +0530

    Largestnumber

---
+++ src/Largestnumber.java
@@ -0,0 +1,21 @@
+
+public class Largestnumber {
+	public static void main(String[] args) {
+
+		int number1 = 24;
+		int number2 = 20;
+		int number3 = 1;
+
+		if (number1 >= number2 && number1 >= number3) {
+			System.out.println(number1 + " is the biggest");
+		} else if (number2 >= number1 && number2 >= number3) {
+			System.out.println(number2 + " is the biggest");
+		} else {
+			System.out.println(number3 + " is the biggest");
+		}
+
+
+}
+
+
+}
